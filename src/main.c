7356af8eb9f87bcf9c9d02f71b7428ce3307bee2
Jordano Iqbal Darmawan <79011617+jordanoid@users.noreply.github.com>
2025-10-25 19:58:04 +0700

    adjustment for uart shell

--- src/main.c
+++ src/main.c
@@ -10,15 +10,18 @@ static const struct gpio_dt_spec internal_led_spec = GPIO_DT_SPEC_GET(DT_ALIAS(l
 
 int main(void)
 {
-	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-	uint32_t dtr = 0;
 	int ret;
 
-	/* Poll if the DTR flag was set */
-	// while (!dtr) {
-	// 	uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-	// 	k_sleep(K_MSEC(100));
-	// }
+	/*
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	// Poll if the DTR flag was set
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+	*/
 
 	ret = gpio_pin_configure_dt(&internal_led_spec, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
@@ -27,8 +30,6 @@ int main(void)
     }
 
 	while (1) {
-
-		printk("Hello World! %s\n", CONFIG_ARCH);
 		ret = gpio_pin_toggle_dt(&internal_led_spec);
 		if (ret < 0) {
             printk("Error toggling pin: %d\n", ret);
